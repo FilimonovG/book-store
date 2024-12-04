30e6684a31bc72af5257342ce5bde3d5ce6ecd76
Fly_Grohh <egohka@mail.ru>
2024-12-04 15:42:54 +0300

    update

---
+++ client/src/pages/Book/Book.jsx
@@ -0,0 +1,14 @@
+import './Book.css'
+import Layout from "../../components/Layout/Layout";
+
+function Book(){
+    return(
+        <Layout>
+            <div className={"book-page"}>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default Book
